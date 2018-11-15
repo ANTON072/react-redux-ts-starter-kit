e490defd8a7ba84c4f47a999388be8dd7471868c
ougi <bdbkx286@gmail.com>
2018-11-15 16:40:23 +0900

    cssの読み込み順修正

--- src/index.tsx
+++ src/index.tsx
@@ -7,8 +7,8 @@ import ReactDOM from "react-dom"
 import { BrowserRouter } from "react-router-dom"
 import { PersistGate } from "redux-persist/integration/react"
 import { Provider } from "react-redux"
-import "./global.css"
 import "normalize.css"
+import "./global.css"
 
 const { store, persistor } = configureStore()
 
